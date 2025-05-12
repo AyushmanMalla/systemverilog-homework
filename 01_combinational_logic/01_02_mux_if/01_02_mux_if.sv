a71e8e96f4e4be31c22551e5c38b124b55c5c099
A.Malla <mall0006@e.ntu.edu.sg>
2025-05-12 17:40:46 +0800

    q2 solveed. added new learning note

--- 01_combinational_logic/01_02_mux_if/01_02_mux_if.sv
+++ 01_combinational_logic/01_02_mux_if/01_02_mux_if.sv
@@ -31,6 +31,36 @@ module mux_4_1
   // Task:
   // Using code for mux_2_1 as an example,
   // write code for 4:1 mux using the "if" statement
+always_comb
+  if (sel == 2'b00) y = d0;
+  else if (sel == 2'b01) y = d1;
+  else if (sel == 2'b10) y = d2;
+  else y = d3;
 
 
 endmodule
+
+
+/*
+    Things to note: 
+    verilog follows the same logic as C/Cpp 
+    if multiple statements need to be written inside each of the
+    if else loops, we need to enclose them in {} [C/C++]
+    if else loops, we need to use a begin/end syntax [verilog, systemVerilog]
+
+    eg:
+    C/C++ 
+      if (condition) {
+          //multi line instructions
+      }
+
+      if (condition) //single line instruction
+
+    Verilog/ SV
+
+        if (condition) begin
+          //multi line instructions
+        end
+        
+        if (condition) //single line instruction
+*/
